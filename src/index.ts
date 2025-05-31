29a90cb517747e8c34dd8124555019a883f40368
frihu <127482729+feikukuai@users.noreply.github.com>
2025-06-01 04:43:30 +0800

    更新 index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
-import Extension from './include/plugin'
+import Extension from './include/plugin';
 
 new Extension({
-    id: "ExampleExtension",
-    name: "example",
-    color1: "#0800ff",
+    id: "MoveExtension",
+    name: "移动",
+    color1: "#00BFFF", // 蓝色调
     blocks: [
         {
-            opcode: 'output',
+            opcode: 'moveSteps',
             blockType: 'command',
-            text: '命令 [block:string] [type:string]',
-            default: { block : "参数2", type: "类型" },
-            menu: { block : [ "参数1", "参数2" ] },
-            bind: function({ block, type }) {
-                return `内容 ${block} 类型 ${type}`;
-            },
-        }, {
-            opcode: 'list',
+            text: '移动 [steps:number] 步',
+            default: { steps: 10 },
+            bind: ({ steps }, util) => {
+                util.target.x += steps * Math.cos(util.target.direction * Math.PI / 180);
+                util.target.y += steps * Math.sin(util.target.direction * Math.PI / 180);
+            }
+        },
+        {
+            opcode: 'setPosition',
+            blockType: 'command',
+            text: '设置位置为 x: [x:number] y: [y:number]',
+            default: { x: 0, y: 0 },
+            bind: ({ x, y }, util) => {
+                util.target.x = x;
+                util.target.y = y;
+            }
+        },
+        {
+            opcode: 'pointDirection',
+            blockType: 'command',
+            text: '面向 [direction:number] 度',
+            default: { direction: 90 },
+            bind: ({ direction }, util) => {
+                util.target.direction = direction;
+            }
+        },
+        {
+            opcode: 'getX',
             blockType: 'reporter',
-            text: '获取一个空列表',
-            bind: () => [],
-            disableMonitor: true,
+            text: '当前的 X 坐标',
+            bind: (args, util) => util.target.x
+        },
+        {
+            opcode: 'getY',
+            blockType: 'reporter',
+            text: '当前的 Y 坐标',
+            bind: (args, util) => util.target.y
         },
     ]
 }).register();
